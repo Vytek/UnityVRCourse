781a0e05f02fe03575a2ccc68d9ec234484b4e01
Adan Gutierrez <hevyw8@gmail.com>
2016-10-12 17:56:22 -0500

    Implemented raycasting intercatibility with controller and camera-eye

---
+++ Assets/_Scripts/Interactable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+	private Color startColor;
+	public bool targeted = false;
+	private Material material;
+	
+	void Start () 
+	{
+		material = GetComponent<Renderer>().material;
+		startColor = material.color;
+	}
+	
+	
+	void Update () 
+	{
+		if (targeted)
+		{
+			Target();
+			targeted = false;
+		} else
+		{
+			Untarget();
+		}
+	}
+
+	public void Target()
+	{
+		material.color = Color.green;
+	}
+
+	public void Untarget()
+	{
+		material.color = startColor;
+	}
+}
---
+++ Assets/_Scripts/LaserPointer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserPointer : MonoBehaviour {
+
+	GameObject hitObject;
+	RaycastHit hit;
+	
+	
+	void Update () 
+	{
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+		{
+			hitObject = hit.transform.gameObject;
+			Interactable interactable = hitObject.GetComponent<Interactable>();
+			if(interactable != null)
+			{
+				interactable.targeted = true;
+			}
+		}
+	}
+}
