6e4005022752c047128a98bab9f869305c4a174d
Adan Gutierrez <hevyw8@gmail.com>
2016-10-12 19:29:22 -0500

    implemented reticle, and raycasted reticle transform

--- Assets/_Scripts/LaserPointer.cs
+++ Assets/_Scripts/LaserPointer.cs
@@ -5,18 +5,22 @@ public class LaserPointer : MonoBehaviour {
 
 	GameObject hitObject;
 	RaycastHit hit;
-	
+	public Reticle reticle;
 	
 	void Update () 
 	{
 		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
 		{
+			reticle.SetPosition(hit);
 			hitObject = hit.transform.gameObject;
 			Interactable interactable = hitObject.GetComponent<Interactable>();
 			if(interactable != null)
 			{
 				interactable.targeted = true;
 			}
+		} else
+		{
+			reticle.SetPosition();
 		}
 	}
 }
